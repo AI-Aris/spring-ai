99ed685c4a8423f2c0b0870d902e6020d15f9261
arisromil <arisromil@yahoo.com>
2025-02-19 17:25:02 -0700

    initial setup

---
+++ src/test/java/com/example/spring/ai/spring/ai/SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.ai.spring.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
